3e7ed02830837f7aee66c3a941fe49c1abc65f57
cash-luv <mino210125@naver.com>
2023-03-15 17:00:49 +0900

    map 실습 2

--- src/day15_20230314/StudentDTO.java
+++ src/day15_20230314/StudentDTO.java
@@ -2,47 +2,39 @@ package day15_20230314;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class StudentDTO {
-	Map<Integer , String> Student = new HashMap<>();
-	int sno;
-	String name;
-	String major;
-	String mobile;
+	private Long id; // 관리번호
+	private String sno; //학번
+	private String name; //이름
+	private String major; //전공
+	private String mobile; //전화번호
 	
 	public StudentDTO() {
 		
 	}
-
-	public int getSno() {
+	public String getSno() {
 		return sno;
 	}
-
-	public void setSno(int sno) {
-		this.sno = sno;
-		
+	public void setSno(String sno) {
+		this.sno = sno ;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getMajor() {
 		return major;
 	}
-
 	public void setMajor(String major) {
 		this.major = major;
 	}
-
 	public String getMobile() {
 		return mobile;
 	}
-
 	public void setMobile(String mobile) {
 		this.mobile = mobile;
 	}
@@ -50,5 +42,36 @@ public class StudentDTO {
 		String str = sno+"\t"+name+"\t"+major+"\t\t"+mobile;
 		
 		return str;
+	}
+	
+
+//	public String toString() {
+//		return "StudentDTO [id=" + id + ", sno=" + sno + ", name=" + name + ", major=" + major + ", mobile=" + mobile
+//				+ "]";
+//	}
+
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, major, mobile, name, sno);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StudentDTO other = (StudentDTO) obj;
+		return Objects.equals(id, other.id) && Objects.equals(major, other.major)
+				&& Objects.equals(mobile, other.mobile) && Objects.equals(name, other.name)
+				&& Objects.equals(sno, other.sno);
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
 	}
 }
--- src/day15_20230314/StudentMain.java
+++ src/day15_20230314/StudentMain.java
@@ -7,35 +7,51 @@ public class StudentMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		StudentDTO s = new StudentDTO();
-		Map< Integer , String > map2 = new HashMap<>();
+		// 학생정보를 관리할 Map
 		
-		s.setSno(1111);
+		Map< Long , StudentDTO > studentMap = new HashMap<>();
+		StudentDTO s = new StudentDTO();
+		s.setId(1L);
+		s.setSno("1111");
 		s.setName("김자바");
 		s.setMajor("컴퓨터공학");
 		s.setMobile("010-1111-1111");
 		
-		map2.put(1, s.toString());
+		studentMap.put(s.getId(),s);
 		
-		s.setSno(2222);
-		s.setName("이자바");
-		s.setMajor("경영학");
-		s.setMobile("010-2222-2222");
+		StudentDTO s2 = new StudentDTO();
+		s2.setId(1L);
+		s2.setSno("1111");
+		s2.setName("김자바");
+		s2.setMajor("컴퓨터공학");
+		s2.setMobile("010-1111-1111");
+		//두개의 객체를 비교를 할 때 DTO에서 해쉬코드 equals 를 작성해줘야함
+		//두 객체의 필드값이 모두 일치하는지 판단하려면 hashCode(), equals() 메서드를 재정의
+		System.out.println(s.equals(s2));
+		System.out.println(s.getId().equals(s2.getId()));
 		
-		map2.put(2, s.toString());
 		
-		s.setSno(3333);
-		s.setName("박자바");
-		s.setMajor("전자공학");
-		s.setMobile("010-3333-3333");
+		s2.setId(2L);
+		s2.setSno("2222");
+		s2.setName("이자바");
+		s2.setMajor("경영학");
+		s2.setMobile("010-2222-2222");
+		studentMap.put(s2.getId(),s2);
 		
-		map2.put(3, s.toString());
+		StudentDTO s3 = new StudentDTO();
+		s3.setId(3L);
+		s3.setSno("3333");
+		s3.setName("박자바");
+		s3.setMajor("전자공학");
+		s3.setMobile("010-3333-3333");
 		
+		studentMap.put(s3.getId(),s3);
+		//반복문으로 출력
 		System.out.println("id\t학번\t이름\t학과\t\t핸드폰 번호");
 		System.out.println("----------------------------------------------------------------------");
-		for(int key : map2.keySet()) {
-			System.out.print(key+"\t");
-			System.out.println(map2.get(key));
+		for(Long id : studentMap.keySet()) {
+			System.out.print(id+"\t");
+			System.out.println(studentMap.get(id)); //student 객체
 		}
 	}
 
